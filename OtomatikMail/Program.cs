f66f7fd2525c8ad13f54e30b727cc308a8b27e31
nurhat36 <148990405+nurhat36@users.noreply.github.com>
2024-08-01 15:06:24 +0300

    son

--- OtomatikMail/Program.cs
+++ OtomatikMail/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +15,32 @@ namespace OtomatikMail
         static void Main(string[] args)
         {
             string cs = "Data Source=DESKTOP-T11FMIO;Initial Catalog=master;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-            string sql = "";
+            string sql = @"select * from Orders where OrderDate>=DATEADD(DAY,-11720,CONVERT(date,sysdatetime()))";
+            SqlDataAdapter sda=new SqlDataAdapter(sql,cs);
+            DataTable dt=new DataTable();
+            sda.Fill(dt);
+
+            string mailBody = "";
+            foreach (DataRow dr in dt.Rows)
+            {
+                mailBody += dr["OrderDate"]+" " + dr["CustomerID"];
+            }
+            mailGonder(mailBody);
+        }
+
+        private static void mailGonder(string mailBody)
+        {
+            MailMessage eposta = new MailMessage();
+            eposta.From = new MailAddress("nihatbaydag@gmail.com"); 
+            eposta.To.Add("baydagnurhat31@gmail.com");
+            eposta.Subject = "son siparişler";
+            eposta.Body = mailBody;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new System.Net.NetworkCredential("nihatbaydag@gmail.com", "nurhatbaydag36");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.Send(eposta);
         }
     }
 }
